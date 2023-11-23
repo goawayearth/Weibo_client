520299a313f2787f1aa5e8976214aee1f484c81d
TMML <tml3200035928@outlook.com>
2023-11-23 16:26:58 +0800

    as

---
+++ client/src/main/java/login/trysw.java
@@ -0,0 +1,14 @@
+package login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trysw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trysw);
+    }
+}
